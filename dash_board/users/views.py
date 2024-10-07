9b47909e455efdd4af3d254150015b299ce49c29
sskussev <48334401+sskussev@users.noreply.github.com>
2024-10-07 17:40:03 +0300

    SignUp

--- dash_board/users/views.py
+++ dash_board/users/views.py
@@ -1,11 +1,19 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 from .forms import CustomUserCreationForm
 
 
-class SignUp(CreateView):
+# class SignUp(CreateView):
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy('board:index')
+#     template_name = 'users/signup.html'
+
+class SignUp(UserPassesTestMixin, CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('board:index')
     template_name = 'users/signup.html'
 
+    def test_func(self):
+        return self.request.user.is_staff
